fe86b6216573d87b2158907822ab37b6b8cb4c72
[Saurabh Pande] <[sp832154@gmail.com]>
2023-06-15 11:46:59 +0530

    favourate and cart counter is added

--- src/components/pageComponents/Header/Header.js
+++ src/components/pageComponents/Header/Header.js
@@ -1,32 +1,30 @@
 // { This component displays the moonstride logo on the homepage ,so we can reuse this component }
 
-import  { Container, Image } from "react-bootstrap";
+import { useState } from "react";
+import { Container, Image } from "react-bootstrap";
 import { useNavigate, Link } from "react-router-dom";
 // import carticon from "../../../../public/images/carticon"
 
-
 import Styles from "./Header.module.scss";
 
-const Header = () => { 
+const Header = () => {
+  const [notificationCount, setNotificationCount] = useState(0);
+
   return (
-    <header className={Styles['topbar']}>
-    
-      <Container>
-        <Link to={"/listing"}>
-
-            {/* The logo image will be displayed here */}
-            <Image src={process.env.PUBLIC_URL + '/images/icon-36x36.png'} alt="Moonstride Logo" />
-
-        </Link>
-        <div className={`${Styles.cartIcon}`} >
-          <Image src= {process.env.PUBLIC_URL + '/images/favourateicon.svg'}  alt="favourate Icon" width={"25px"}/>
-        
-          <Image src= {process.env.PUBLIC_URL + '/images/carticon.svg'}  alt="Cart Icon" width={"25px"} />
-        </div>
-        
-      </Container>
-    </header>
-    
+  <header className={Styles["topbar"]}>
+  <Container>
+    <Link to={"/listing"}>
+      {/* The logo image will be displayed here */}
+      <Image src={process.env.PUBLIC_URL + "/images/icon-36x36.png"} alt="Moonstride Logo" />
+    </Link>
+    <div className={`${Styles.cartIcon}`}>
+      <Image src={process.env.PUBLIC_URL + "/images/favourateicon.svg"} alt="favourate Icon" width={"25px"} /> 
+      <p className="faviconCount">{notificationCount}</p> 
+      <Image src={process.env.PUBLIC_URL + "/images/carticon.svg"} alt="Cart Icon" width={"25px"} />
+      <p className={`${Styles.cartCount}`}>{notificationCount}</p> 
+    </div>
+  </Container>
+  </header>
   );
 };
 
