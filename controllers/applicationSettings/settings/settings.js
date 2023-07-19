3f97339b13a865d2ddff6676c7329d711616feed
mohammed-shaheer-c <shanshaheer3@gmail.com>
2023-07-19 19:21:07 +0530

    get settings page is done

--- controllers/applicationSettings/settings/settings.js
+++ controllers/applicationSettings/settings/settings.js
@@ -10,15 +10,24 @@ exports.updateSettings = async(req,res)=>
 {
     let settingsPage = await settings.updateSettings(req.body,req.files,req.parms.id);
     console.log("new",settingsPage);
-//    if(settingsPage){
-//     return res.status(200).send
-//     ({
-//         code: 200,
-//         success: true,
-//         message: "Successfully fetched service providers",
-//         data : getAllSProviders
-//     });
-//    }
+    if(settingsPage.settingsPageData == 'NOTFOUND'){
+        return res.status(200).send
+        ({
+            code: 400,
+            success: false,
+            message: constants.responseMessage.getOneErr
+         
+        });
+       }else{
+        return res.status(200).send
+        ({
+            code: 200,
+            success: true,
+            message: constants.responseMessage.getOne,
+            data : settingsPage
+         
+        });
+       }
 }
 
 
