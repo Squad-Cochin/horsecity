c326bf40585cc96de83841da0a2f56cbd08cac86
Saurabh <sp832154@gmail.com>
2023-08-03 18:51:17 +0530

    res.header authentication code is added

--- routes/auth/auth.route.js
+++ routes/auth/auth.route.js
@@ -2,7 +2,6 @@ const authcontroller = require(`../../controllers/auth/auth.controller`);   // i
 // const check?Input = require(`../middlewares/requestValidator`); // Importing the requestValidator file data 
 const checkInput = require(`../../middlewares/validateInput/checkRequestBodyInput`);
 const constants = require("../../utils/constants");
-const http = require('http');
 
 const checkHeaders = require('../../middlewares/validateInput/checkRequestHeader');
 
@@ -10,9 +9,7 @@ module.exports = function(app)
 {
     // The below route is for login of service provider user.
     app.post(`/${process.env.apiToken}/login`,
-    // checkHeaders.checkRequestHeader,
-    // checkInput.usernameValidation(constants.tableName.service_providers),
-    // checkInput.passwordValidation,
+
     authcontroller.serviceProviderLogin);
     
     // app.post(`/${process.env.apiToken}/login`, (req, res)=>{ console.log("Came here"); });
@@ -32,33 +29,5 @@ module.exports = function(app)
     // checkInput.passwordValidation,
     authcontroller.serviceProviderLogout); 
 
-    // app.post(`/${process.env.apiToken}/recoveryPassword`,checkInput.emailvalidation, authcontroller.resetPasswordUsingEmail);  
-
-    const http = require('http');
-
-    http.get('http://localhost:8000/horsecity661809/login', (response) => {
-      // Check if the 'role' header is present
-      const roleHeaderValue = response.headers['role'];
-      if (roleHeaderValue !== undefined) {
-        console.log('Role Header Value:', roleHeaderValue);
-      } else {
-        console.log('Role Header not set.');
-      }
-    
-      // Process the response data
-      let responseData = '';
-      response.on('data', (chunk) => {
-        responseData += chunk;
-      });
-    
-      response.on('end', () => {
-        console.log('Response Data:', responseData);
-      });
-    }).on('error', (error) => {
-      console.error('Error:', error.message);
-    });
-    
-      
-       
-
+    // app.post(`/${process.env.apiToken}/recoveryPassword`,checkInput.emailvalidation, authcontroller.resetPasswordUsingEmail);     
 }
