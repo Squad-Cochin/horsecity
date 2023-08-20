20673de173529eb365866ff1675a2de7fcf95adc
Mohammed-Shaheer-1 <shaheershan3@gmail.com>
2023-08-21 00:32:08 +0530

    code updated

--- models/vehicles/vehicle.model.js
+++ models/vehicles/vehicle.model.js
@@ -593,5 +593,193 @@ module.exports = class vehicles
         }
     };
 
+    // static async getvehicledetailforcustomerpage(Id)
+    // {
+    //     try
+    //     {
+    //         return await new Promise(async(resolve, reject)=>
+    //         {
+    //             // let selQuery = `SELECT v.id, v.service_provider_id, s.name AS service_provider_name, v.vehicle_number, v.price, v.no_of_horse, v.length, v.height, v.breadth, v.make, v.model, v.air_conditioner, v.temperature_manageable, v.gcc_travel_allowed, v.insurance_cover, v.vehicle_type, v.vehicle_registration_date, v.vehicle_exipration_date, v.insurance_date, v.insurance_expiration_date, v.insurance_provider, vi.id AS vehicle_image_id, vi.image, vi.title, r.id AS review_id, r.vehicle_rating, r.vehicle_description, r.created_at
+    //             // FROM vehicles v
+    //             // INNER JOIN service_providers s ON v.service_provider_id = s.id
+    //             // LEFT JOIN vehicles_images vi ON v.id = vi.vehicle_id AND vi.status = "${constants.status.active}" 
+    //             // LEFT JOIN reviews r ON v.id = r.vehicle_id
+    //             // WHERE v.id = ${Id}
+    //             // AND v.status = "${constants.status.active}"`;
+
+    //             let selQuery = `SELECT
+    //                             v.id,
+    //                             v.service_provider_id,
+    //                             s.name AS service_provider_name,
+    //                             v.vehicle_number,
+    //                             v.price,
+    //                             v.no_of_horse,
+    //                             v.length,
+    //                             v.height,
+    //                             v.breadth,
+    //                             v.make,
+    //                             v.model,
+    //                             v.air_conditioner,
+    //                             v.temperature_manageable,
+    //                             v.gcc_travel_allowed,
+    //                             v.insurance_cover,
+    //                             v.vehicle_type,
+    //                             v.vehicle_registration_date,
+    //                             v.vehicle_exipration_date,
+    //                             v.insurance_date,
+    //                             cr.abbreviation,
+    //                             v.insurance_expiration_date,
+    //                             v.insurance_provider,
+    //                             vi.id AS vehicle_image_id,
+    //                             vi.image,
+    //                             vi.title,
+    //                             r.id AS review_id,
+    //                             r.vehicle_description,
+    //                             SUM(CASE WHEN r.vehicle_rating = 1 THEN 1 ELSE 0 END) AS rating_1_count, 
+    //                             SUM(CASE WHEN r.vehicle_rating = 2 THEN 1 ELSE 0 END) AS rating_2_count, 
+    //                             SUM(CASE WHEN r.vehicle_rating = 3 THEN 1 ELSE 0 END) AS rating_3_count, 
+    //                             SUM(CASE WHEN r.vehicle_rating = 4 THEN 1 ELSE 0 END) AS rating_4_count, 
+    //                             SUM(CASE WHEN r.vehicle_rating = 5 THEN 1 ELSE 0 END) AS rating_5_count, 
+    //                             COUNT(r.id) AS review_count,  
+    //                             r.created_at,
+    //                             c.name AS customer_name
+    //                             FROM vehicles v
+    //                             INNER JOIN service_providers s ON v.service_provider_id = s.id
+    //                             JOIN ${constants.tableName.bookings} b ON v.service_provider_id = b.service_provider_id  
+    //                             JOIN ${constants.tableName.customers} c ON b.customer_id  = c.id  
+    //                             LEFT JOIN currencies cr ON cr.id = ( SELECT currency_id FROM application_settings WHERE application_settings.currency_id = cr.id )
+    //                             LEFT JOIN vehicles_images vi ON v.id = vi.vehicle_id AND vi.status = "${constants.status.active}"
+    //                             LEFT JOIN reviews r ON v.id = r.vehicle_id
+    //                             WHERE v.id = ${Id} AND v.status = "${constants.status.active}" `;
+    //             // console.log(`Fetch data query: `, selQuery);
+    //             con.query(selQuery, (err, result) =>
+    //             {
+    //                 // console.log(result);
+    //                 // console.log(`Result of the particular vehicle for the customer page: `, result);
+    //                 if(err)
+    //                 {
+    //                     console.log(err);
+    //                     resolve('err');
+    //                 }
+    //                 else
+    //                 {
+    //                     var vehicleResponse =
+    //                     {
+    //                         "vehicle": [],
+    //                         "images" : [],
+    //                         "reviews": [],
+    //                         "review_ratings" : []
+    //                     };
+    //                     if(result.length != 0)
+    //                     {
+    //                         const uniqueReviewIds = new Set();
+    //                         const uniqueImagesIds = new Set();
+                    
+    //                         for (let row of result)
+    //                         {
+    //                             if (vehicleResponse.vehicle.length === 0)
+    //                             {
+    //                                 // If result has data, populate the vehicleResponse data
+    //                                 vehicleResponse.vehicle.push
+    //                                 ({
+    //                                     "service_provider_id" : result[0].service_provider_id,
+    //                                     "service_provider_name" : result[0].service_provider_name,
+    //                                     "vehicle_number" : result[0].vehicle_number,
+    //                                     "price" : result[0].price,
+    //                                     "no_of_horses" : result[0].no_of_horse,
+    //                                     "length" : result[0].length,
+    //                                     "breadth" : result[0].breadth,
+    //                                     "height" : result[0].height,
+    //                                     "make" : result[0].make,
+    //                                     "model" : result[0].model,
+    //                                     "air_condition" : result[0].air_conditioner,
+    //                                     "temperature_manageable" : result[0].temperature_manageable,
+    //                                     "gcc_travel_allowed" : result[0].gcc_travel_allowed,
+    //                                     "insurance_cover" : result[0].insurance_cover,
+    //                                     "insurance_provider" : result[0].insurance_provider,
+    //                                     "vehicle_type" : result[0].vehicle_type,
+    //                                     "abbreviation" : result[0].abbreviation,
+    //                                     "vehicle_registration_date" : time.formatDateToDDMMYYYY(result[0].vehicle_registration_date),
+    //                                     "vehicle_expiration_date" : time.formatDateToDDMMYYYY(result[0].vehicle_exipration_date),
+    //                                     "insurance_date" : time.formatDateToDDMMYYYY(result[0].insurance_date),
+    //                                     "insurance_expiration_date" : time.formatDateToDDMMYYYY(result[0].insurance_expiration_date)
+    //                                 });
+    //                             }
+
+    //                             if(row.vehicle_image_id !== null && !uniqueImagesIds.has(row.vehicle_image_id))
+    //                             {
+    //                                 vehicleResponse.images.push
+    //                                 ({
+    //                                     "id": row.vehicle_image_id,
+    //                                     "url" : `${process.env.PORT_SP}${constants.attachmentLocation.vehicle.view.image}${row.image}`
+    //                                 });
+    //                                 uniqueImagesIds.add(row.vehicle_image_id);
+    //                             }                           
+
+    //                             if (row.review_id !== null && !uniqueReviewIds.has(row.review_id)) 
+    //                             {
+    //                                 vehicleResponse.reviews.push
+    //                                 ({
+    //                                     "id": row.review_id,
+    //                                     "customer_name" : row.customer_name,
+    //                                     // "rating" : row.vehicle_rating,
+    //                                     "description" : row.vehicle_description,
+    //                                     "created_at" : time.formatDateToDDMMYYYY(row.created_at)
+    //                                 });
+
+    //                                 // const reviewRatings = [];
+    //                                 // for (let rating = 1; rating <= 5; rating++) {
+    //                                 //     reviewRatings.push({
+    //                                 //         "rating": rating,
+    //                                 //         "rating_count": row[`rating_${rating}_count`], // Use the dynamically generated column name
+    //                                 //         "review_count": row.review_count
+    //                                 //     });
+    //                                 // }
+    //                                 const reviewRatings = [
+    //                                     {
+    //                                         "rating": 1,
+    //                                         "rating_count": row.rating_1_count,
+    //                                         "review_count": row.review_count
+    //                                     },
+    //                                     {
+    //                                         "rating": 2,
+    //                                         "rating_count": row.rating_2_count,
+    //                                         "review_count": row.review_count
+    //                                     },
+    //                                     {
+    //                                         "rating": 3,
+    //                                         "rating_count": row.rating_3_count,
+    //                                         "review_count": row.review_count
+    //                                     },
+    //                                     {
+    //                                         "rating": 4,
+    //                                         "rating_count": row.rating_4_count,
+    //                                         "review_count": row.review_count
+    //                                     },
+    //                                     {
+    //                                         "rating": 5,
+    //                                         "rating_count": row.rating_5_count,
+    //                                         "review_count": row.review_count
+    //                                     }
+                                      
+    //                                 ];
+    //                                 vehicleResponse.review_ratings = reviewRatings;
+
+    //                                 uniqueReviewIds.add(row.review_id);
+    //                             }
+    //                         }
+    //                         // console.log(`Final Data: `, vehicleResponse);
+    //                         resolve(vehicleResponse);
+    //                     }
+    //                 }
+    //             });            
+    //         });
+    //     }
+    //     catch (error)
+    //     {
+    //         return console.log(`Error from the vehicle.model.js file from the models > vehicles folder. In the static function "getvehicledetailforcustomerpage". Which is designed to fetch the details of a particular vehicle for the details page in the customer side`);                        
+    //     }
+    // };
+
 };
 
