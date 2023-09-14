42ff51db72e4597e646f817c7a8ab2eba9a2ba4f
Saurabh Pande <sp832154@gmail.com>
2023-09-14 12:02:35 +0530

    Messages are updated in this file

--- controllers/dashboard/dashboard.controller.js
+++ controllers/dashboard/dashboard.controller.js
@@ -11,7 +11,7 @@
 
 
 const dashboardData = require(`../../models/dashboard/dashboard.model`); // The model from where the logic is intantiate are written in dashboard model
-
+const constant = require('../../utils/constants');
 
 // The below will fetching the total counts for admin and service providers
 /**
@@ -29,22 +29,20 @@ exports.getDashboardDataForParticularProvider = async (req, res, next) =>
     const data = await dashboardData.getdashboarddataforparticularprovider(req.params.id)
     if(data === 'err')
     {
-        console.log(`Error while fetching the data for the dashboard`);
         res.status(200).send
         ({
             code : 500,
             status : false,
-            message : "Internal server error while fetching the dashbord data"
+            message: constant.responseMessage.universalError
         });
     }
     else
     {
-        // console.log(`Dashboard data fetched successfully`);
         res.status(200).send
         ({
             code : 200,
             status : true,
-            message : "Dashboard data fetched successfully",
+            message : constant.responseMessage.getAll,
             data : data
         });
     }
@@ -56,12 +54,11 @@ exports.getMontlySalesReport = async (req, res, next) =>
     const data = await dashboardData.getmonthlysalesreport(req.params.id);
     if(data === 'err')
     {
-        console.log(`Error while fetching the monthly sales report`);
         res.status(200).send
         ({
             code : 500,
             status : false,
-            message : "Internal server error while fetching the monthly sales report"
+            message: constant.responseMessage.universalError
         });
     }
     else
@@ -71,7 +68,7 @@ exports.getMontlySalesReport = async (req, res, next) =>
         ({
             code : 200,
             status : true,
-            message : "Monthly sales data fetched successfully",
+            message : constant.responseMessage.getAll,
             data : data
         });
     }
@@ -83,22 +80,20 @@ exports.getDashboardQuotationStatus = async (req, res, next) =>
     const data = await dashboardData.getdashboardquotationstatus(req.params.id);
     if(data === 'err')
     {
-        console.log(`Error while fetching the quotation data for the dashboard`);
         res.status(200).send
         ({
             code : 500,
             status : false,
-            message : "Internal server error while fetching the quotation data for the dashboard"
+            message: constant.responseMessage.universalError
         });
     }
     else
     {
-        console.log(`Quotation data for the dashboard fetched successfully`);
         res.status(200).send
         ({
             code : 200,
             status : true,
-            message : "Quotation data for the dashboard fetched successfully fetched successfully",
+            message : constant.responseMessage.getAll,
             data : data
         });
     }
@@ -110,22 +105,21 @@ exports.getLatestEnquiries = async (req, res) =>
     const data = await dashboardData.getlatestenquiries(req.params.id);
     if(data === 'err')
     {
-        console.log(`Error while fetching the latest enquiries for the dashboard`);
         res.status(200).send
         ({
             code : 500,
             status : false,
-            message : "Internal server error while fetching the latest enquiries data for the dashboard"
+            message: constant.responseMessage.universalError
         });
     }
     else
     {
-        console.log(`Latest enquiries data for the dashboard fetched successfully `);
+        // console.log(`Latest enquiries data for the dashboard fetched successfully `);
         res.status(200).send
         ({
             code : 200,
             status : true,
-            message : "Latest enquiries data for the dashboard fetched successfully ",
+            message: constant.responseMessage.getAll,
             data : data
         });
     }
